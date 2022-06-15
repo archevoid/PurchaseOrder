ed1ed8db24ff006ee97fa85260c9eef04a8cf6bf
jicmu <jicmu0@gmail.com>
2022-06-15 20:57:38 +0900

    기능 추가: 회원가입 기능 (미구현: 비밀번호 확인 체크, 부서 DB에서 자동 출력)

---
+++ src/main/java/com/mit/model/EpleeDTO.java
@@ -0,0 +1,14 @@
+package com.mit.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EpleeDTO extends UserDTO {
+	
+	private String email;
+	private Integer deptNum;
+	
+	
+}
--- src/main/java/com/mit/model/UserDAO.java
+++ src/main/java/com/mit/model/UserDAO.java
@@ -2,4 +2,7 @@ package com.mit.model;
 
 public interface UserDAO {
 	UserDTO getUserById(String userid);
+	
+	int doRegistUser(UserDTO userDTO);
+	int doRegistEmpl(UserDTO userDTO);
 }
