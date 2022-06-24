a89f181bb8b37baa374a952e183b28753f3061d7
Jicmu <Jicmu@jicmu.org>
2022-06-24 12:48:48 +0900

    DOCS: DB 수정 및 ERD 추가

--- SQL/progress.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `progress` (
-	`plan_num` INT(11) NOT NULL,
-	`current_quantity` INT(11) NOT NULL DEFAULT '0',
-	INDEX `FK_progress_plan` (`plan_num`) USING BTREE,
-	CONSTRAINT `FK_progress_plan` FOREIGN KEY (`plan_num`) REFERENCES `purchaseorder`.`plan` (`plan_num`) ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-COLLATE='utf8mb3_general_ci'
-ENGINE=InnoDB
-;
