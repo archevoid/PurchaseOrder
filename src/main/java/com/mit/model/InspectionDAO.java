9dc2b06c43ffa1af9d1f3f4e67d80349cddeac99
Jicmu <Jicmu@jicmu.org>
2022-07-11 21:04:21 +0900

    Chore: SQL Service 추가

--- src/main/java/com/mit/model/InspectionDAO.java
+++ src/main/java/com/mit/model/InspectionDAO.java
@@ -6,4 +6,16 @@ public interface InspectionDAO {
 	public List<OrderDTO> getNotFinishedOrder();
 	
 	public List<InspectionDTO> getInspectionByOrderNum(InspectionDTO inspectionDto);
+	
+	public InspectionDTO getInspectionSchedule(InspectionDTO inspectionDto);
+	
+	public InspectionDTO getInspectionResult(InspectionDTO inspectionDto);
+	
+	public Integer insertInspectionSchedule(InspectionDTO inspectionDto);
+	
+	public Integer insertInspectionResult(InspectionDTO inspectionDto);
+	
+	public Integer getNextInspectionNum(InspectionDTO inspectionDto);
+	
+	public Integer getNextInspectionResultNum(InspectionDTO inspectionDto);
 }
