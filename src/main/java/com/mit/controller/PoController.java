a51250c47517595aa9cc2c97d2b58122f2f86801
Jicmu <Jicmu@jicmu.org>
2022-07-20 17:45:45 +0900

    Feat: 발주서 pdf 파일로 변환 기능 추가

--- src/main/java/com/mit/controller/PoController.java
+++ src/main/java/com/mit/controller/PoController.java
@@ -1,11 +1,15 @@
 package com.mit.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.itextpdf.text.Document;
 import com.mit.model.CurPageDTO;
 import com.mit.model.InspectionDTO;
 import com.mit.model.OrderDTO;
@@ -21,6 +26,7 @@ import com.mit.model.PageDTO;
 import com.mit.model.PlanDTO;
 import com.mit.service.InspectionService;
 import com.mit.service.OrderService;
+import com.mit.service.PdfService;
 import com.mit.service.PlanService;
 import com.mit.service.UserService;
 
@@ -35,6 +41,7 @@ public class PoController {
 	OrderService os;
 	InspectionService is;
 	UserService us;
+	
 	
 	/* 파일 저장 경로 (root-context에 선언) */
 	@Resource(name = "pathOfInspectionFile")
@@ -96,7 +103,7 @@ public class PoController {
 	}
 	
 	@PostMapping("printpo")
-	public String printpo(OrderDTO orderDto, HttpServletRequest request, Model m) {
+	public String printpo(OrderDTO orderDto, HttpServletRequest request, HttpServletResponse response, Model m) {
 		String referer = (String)request.getHeader("REFERER");
 		
 		String url = request.getRequestURL().toString().replace(request.getRequestURI(), "");
@@ -114,7 +121,7 @@ public class PoController {
 		currency.put("JPN", '\u00A5');
 		currency.put("CNY", '\u00A5');
 		currency.put("other", '\u00A4');
-		
+
 		
 		m.addAttribute("orderInfo", os.getOrderInfo(orderDto));
 		
