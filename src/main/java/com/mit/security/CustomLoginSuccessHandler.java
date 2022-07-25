f0374c34591091cf405f0c08ab769c4cd747500a
Jicmu <Jicmu@jicmu.org>
2022-07-25 16:35:45 +0900

    Feat: 권한 추가

--- src/main/java/com/mit/security/CustomLoginSuccessHandler.java
+++ src/main/java/com/mit/security/CustomLoginSuccessHandler.java
@@ -32,9 +32,9 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 		if(roleNames.contains("ROLE_PTN")) {
 			response.sendRedirect("/po/partner");
 			return;
-		} else {
-//			response.sendRedirect("/po/dashboard");
-			response.sendRedirect("http://192.168.0.104:8888/index/board");
+		} else if (roleNames.contains("ROLE_PD")) {
+			response.sendRedirect("/po/dashboard");
+//			response.sendRedirect("http://192.168.0.104:8888/index/board");
 			return;
 		}
 	}
